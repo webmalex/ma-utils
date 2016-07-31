54e82ae7c3703a9a6fb62369f74cf0ae01a59667
Alexander Efimov <alexandr.efimov@gmail.com>
2016-07-31 23:46:37 +0500

    from ma_utils import _call as s

--- ma_utils.py
+++ ma_utils.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+# import atexit
 import click
 import sys
-from subprocess import call
+from subprocess import call as subprocess_call
+
+def print_color(msg, color, **kargs):
+    click.echo(click.style(msg, fg=color), **kargs)
+def print_er(msg):
+    print_color(msg, 'red', file=sys.stderr)
+def print_ok(msg):
+    print_color(msg, 'green')
 
 # cli = click.Group()
 @click.group()
@@ -12,38 +20,55 @@ def cli(): pass
 @click.argument('cmd')
 @click.option('--echo/--no-echo', default=True)
 @click.option('--stop/--no-stop', default=True, help='on error')
-def cmd(cmd, echo, stop):
+def call(cmd, echo, stop):
     '''Run cmd'''
-    def print_error(msg):
-        click.echo(click.style(msg, fg='red'), file=sys.stderr)
+    return _call(cmd, echo, stop)
+
+def _call(cmd, echo=True, stop=True):
     if echo:
-        click.echo(click.style(cmd, fg='green'))
+        print_ok(cmd)
     try:
-        code = call(cmd, shell=True)
+        code = subprocess_call(cmd, shell=True)
     except OSError as e:
         code = -1
-        print_error(e)
+        print_er(e)
     if code:
-        print_error("error=%i" % code)
+        print_er("error=%i" % code)
         if stop:
+            # raise
             exit(code)
+    return code
 
 @cli.command()
 @click.pass_context
 def test(ctx):
     ''' Test cmd '''
     from click.testing import CliRunner
-    def _test(task, arg):
+    def run(task, arg):
         result = CliRunner().invoke(task, arg)
         assert result.exit_code == 0
         return result.output
-    assert _test(cmd, ['echo 1']) == 'echo 1\n'
-    assert _test(cmd, ['echo 2', '--no-echo']) == ''
+    assert run(call, ['echo 1']) == 'echo 1\n'
+    assert run(call, ['echo 2', '--no-echo']) == ''
 
-    ctx.invoke(cmd, cmd='echo 1')
-    ctx.invoke(cmd, cmd='echo 2', echo=False)
-    ctx.invoke(cmd, cmd='exit 3', stop=False)
-    ctx.invoke(cmd, cmd='echo 4')
+    ctx.invoke(call, cmd='echo 1')
+    ctx.invoke(call, cmd='echo 2', echo=False)
+    ctx.invoke(call, cmd='exit 3', stop=False)
+    ctx.invoke(call, cmd='exit 4')
+    # try: ctx.invoke(call, cmd='exit 4')
+    # except: print('ok')
+    ctx.invoke(call, cmd='echo 5')
+
+def _test():
+    assert _call('echo 1') == 0
+    assert _call('echo 2', echo=False) == 0
+    assert _call('exit 3', stop=False) == 3
+    assert _call('exit 4') == 0
+    assert _call('echo 5') != 0
+
+# @atexit.register
+# def goodbye():
+#     print_er("stop error in script")
 
 if __name__ == '__main__':
     pass
